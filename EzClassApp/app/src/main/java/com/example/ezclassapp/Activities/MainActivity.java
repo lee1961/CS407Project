65680cb6e15d175b820525f7f2bb801d4d40dfad
lee1961 <lee1961@purdue.edu>
2017-08-23 00:29:51 -0400

    did something

--- EzClassApp/app/src/main/java/com/example/ezclassapp/Activities/MainActivity.java
+++ EzClassApp/app/src/main/java/com/example/ezclassapp/Activities/MainActivity.java
@@ -1,12 +1,17 @@
-package com.example.ezclassapp;
+package com.example.ezclassapp.Activities;
 
 import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
+import com.example.ezclassapp.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -41,8 +46,30 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.main_menu,menu);
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        EditText searchEditText = (EditText) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        searchEditText.setTextColor(getResources().getColor(R.color.material_white));
+        searchEditText.setHintTextColor(getResources().getColor(R.color.material_white));
+        //searchView.setOnQueryTextListener(this);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // whenever the you type something into the search Bar
+                Log.d("clicked action search","changing text");
+                return true;
+            }
+        });
         return true;
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -50,6 +77,8 @@ public class MainActivity extends AppCompatActivity {
         if(item.getItemId() == R.id.main_logout_btn) {
             FirebaseAuth.getInstance().signOut();
             sendToStart();
+        } else if (item.getItemId() == R.id.action_search) {
+            Log.d("clicked action search","manage to click action search");
         }
         return true;
     }
