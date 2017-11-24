0bf875a9f458e9b098b06b03432f8b5bccdc666f
Avinash <aprabhak@purdue.edu>
2017-11-24 17:21:46 -0500

    click notifications

--- EzClassApp/app/src/main/java/com/example/ezclassapp/Helpers/FirebaseMessagingService.java
+++ EzClassApp/app/src/main/java/com/example/ezclassapp/Helpers/FirebaseMessagingService.java
@@ -1,8 +1,11 @@
 package com.example.ezclassapp.Helpers;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 
+import com.example.ezclassapp.Activities.DetailedReviewActivity;
 import com.example.ezclassapp.R;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -18,12 +21,24 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
         String notification_title = remoteMessage.getNotification().getTitle();
         String notification_message = remoteMessage.getNotification().getBody();
 
+        String click_action = remoteMessage.getNotification().getClickAction();
 
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.mipmap.icon4)
                         .setContentTitle("Review update foreground")
                         .setContentText(notification_message);
+
+        Intent resultIntent = new Intent(click_action);
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+
         // Sets an ID for the notification
         int mNotificationId = (int)System.currentTimeMillis();
 // Gets an instance of the NotificationManager service
